37db7445c14e8d10b56360e06cf0489c800e2aa3
Fredbo561 <fredbo561@gmail.com>
2025-02-06 23:54:23 -0500

    worked on schema

---
+++ src/db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employer;
+CREATE DATABASE employer;
+
+\c employer;
+
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name varchar(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL
+);
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER NOT NULL
+);
+
